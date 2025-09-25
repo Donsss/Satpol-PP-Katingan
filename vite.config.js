832db56fcd36c6d5133d5de3211cff599fd697df
Donss <pasep9984@gmail.com>
2025-09-25 15:16:22 +0700

    perbaikkan vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/text-editor.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
 
